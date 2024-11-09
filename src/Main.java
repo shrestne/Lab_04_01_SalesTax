3d9e7fd9fdc2dce9960fd30c325446d03e2a339b
shrestne <iniceshrestha@gmail.com>
2024-11-08 23:33:32 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double priceOfAPurchase = 420.69;
+        double salesTax = 0.05 * priceOfAPurchase;
+        System.out.println("The price of the purchase is "+ priceOfAPurchase + " and the sales tax is "+ salesTax);
+    }
+}
